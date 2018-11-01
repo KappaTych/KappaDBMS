0208dfddefafc60d4d9fc9df42cf525bec9b03b7
Anton Podchuvalov <podchuvalovvl@gmail.com>
2018-11-02 02:47:31 +1000

    Added simple backend

--- src/frontend/kappaweb/urls.py
+++ src/frontend/kappaweb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('query/', views.ActionQuery.index),
 ]
