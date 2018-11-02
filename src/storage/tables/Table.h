90efb39e18608bda50e2b07cea87422a3e79bfe8
truefinch <glushkov.vk@students.dvfu.ru>
2018-11-02 14:07:07 +1000

    + Add class Table + Add class Record + Add class Field All classes now have got constructors. Tables consist of vector of records, that consist of vector of fields. Fields represents by its value and value's data type

--- src/storage/tables/Table.h
+++ src/storage/tables/Table.h
@@ -3,7 +3,28 @@
 //
 #pragma once
 
-class Table {
+#include <string>
+#include <vector>
+#include "Record.h"
 
-};
+namespace tables {
+
+    struct Column {
+        std::string name;
+        dt::DataType dataType;
+    };
+
+    class Table {
+    public:
+        std::string name;
+        std::vector<Record> records;
+
+        explicit Table(std::string name, std::vector<Column> columns);
+
+
+
+    private:
+        std::vector<Column> _columns;
+    };
+} //namespace tables
 
