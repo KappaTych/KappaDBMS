54f2cd06d2173aa706f1a7e0baf1becf54695f43
pototckaia <pototckaia@gmail.com>
2018-11-02 12:05:28 +1000

    minor changes

--- src/backend/parser/dbStub.cpp
+++ src/backend/parser/dbStub.cpp
@@ -3,16 +3,17 @@
 #include <iostream>
 
 void create_table(sql::TableName t, std::vector<sql::ColumnDefinition> v) {
-	std::cout << "CREATE" << t.schema << " " << t.name;
+	std::cout << "Success::CREATE" << t.schema << " " << t.name << std::endl;
 	for (auto &e : v) {
-		std::cout << e.name << " " << e.type;
+		std::cout << e.name << " " << e.type << " ";
 	}
+	std::cout << std::endl;
 }
 
 void drop_table(sql::TableName t) {
-	std::cout << "Drop" << t.schema << " " << t.name;
+	std::cout << "Success::Drop" << t.schema << " " << t.name << std::endl;
 }
 
 void show_table(sql::TableName t) {
-	std::cout << "SHOW" << t.schema << " " << t.name;
+	std::cout << "Success::SHOW" << t.schema << " " << t.name << std::endl;
 }
