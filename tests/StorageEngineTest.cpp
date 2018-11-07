6ea73267d7689f010cf7da97b6c1624f630450e6
nixtaxe <fenix_dz@mail.ru>
2018-11-07 17:49:04 +1000

    Add storage tests

---
+++ tests/StorageEngineTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../src/backend/storage/StorageEngine.h"
+#include "../src/backend/storage/datatypes/object.h"
+
+int main()
+{
+  se::StorageEngine::getInstance().create("test", {{"z",   dt::DataType::INTEGER},
+                                                   {"name", dt::DataType::TEXT}});
+  se::StorageEngine::getInstance().create("test_problem", {{"id", dt::DataType::INTEGER},
+                                                           {"id", dt::DataType::DOUBLE}});
+
+  std::cout << se::StorageEngine::getInstance().show_create("test");
+}
