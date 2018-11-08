e34903f2613d5746ca8938add4f30e76eb24b808
nixtaxe <fenix_dz@mail.ru>
2018-11-08 23:27:35 +1000

    Add select all

--- src/backend/storage/tables/Table.cpp
+++ src/backend/storage/tables/Table.cpp
@@ -5,8 +5,8 @@
 
 #include "Table.h"
 
-tables::Table::Table(std::string name, nlohmann::fifo_map<std::string, dt::DataType> columns) : name(
-    std::move(name)), _columns(columns)
+tables::Table::Table(std::string name, nlohmann::fifo_map<std::string, dt::DataType> columns) : name(std::move(name)),
+                                                                                                _columns(columns)
 {
 }
 
@@ -35,7 +35,7 @@ std::string tables::Table::ToString()
 {
   std::string result;
   result += name + " (";
-  for (auto& col : _columns) {
+  for (auto &col : _columns) {
     result += col.first + " " + std::to_string(col.second) + ", ";
   }
 
@@ -44,3 +44,49 @@ std::string tables::Table::ToString()
   result += ");";
   return result;
 }
+
+namespace tables
+{
+void to_json(my_json &j, const Table &t)
+{
+  std::string result = "{\"name\":\"" + t.name + "\",\"columns\": [";
+
+//  std::vector<std::pair<std::string, dt::DataType>> col;
+
+  std::string prefix = "\"";
+  for (auto &column : t.getColumns()) {
+    result += prefix + column.first;
+    prefix = "\",\"";
+  }
+  result += "\"],\"records\" : {";
+
+  int index = 0;
+  prefix = "";
+  for (auto &record : t.records) {
+    result += prefix + "\"" + std::to_string(index) + "\":" + "[";
+    index++;
+    std::string str = record.get();
+    std::string buffer = "";
+    prefix = "\"";
+    std::string suffix = "\"";
+    char ch;
+    for (int i = 0; i < str.length(); ++i) {
+      ch = str[i];
+      if (ch == tables::DIVIDOR || i + 1 == str.length()) {
+        if (i + 1 == str.length())
+          buffer += ch;
+        result += prefix + buffer + suffix;
+        prefix = ",\"";
+        buffer = "";
+        continue;
+      }
+      buffer += ch;
+    }
+    prefix = ",";
+    result += "]";
+  }
+  result += "}}";
+
+  j = json::parse(result.begin(), result.end());
+}
+} //namespace tables
--- tests/StorageEngineTest.cpp
+++ tests/StorageEngineTest.cpp
@@ -11,11 +11,14 @@ int main()
 //  se::StorageEngine::getInstance().create("test_problem", {{"id", dt::DataType::INTEGER},
 //                                                           {"id", dt::DataType::DOUBLE}});
 
-  se::StorageEngine::getInstance().insert("test", "5~hello");
-  se::StorageEngine::getInstance().insert("test", "6~lolik");
-  se::StorageEngine::getInstance().insert("hehe", "hih");
+//  se::StorageEngine::getInstance().insert("test", "5~hello");
+//  se::StorageEngine::getInstance().insert("test", "6~lolik");
+//  se::StorageEngine::getInstance().insert("hehe", "hih");
+  se::StorageEngine::getInstance().insert("test_problem", "9~hih");
 
 //  std::cout << se::StorageEngine::getInstance().show_create("test") << std::endl;
 //  std::cout << se::StorageEngine::getInstance().show_create("test_problem") << std::endl;
+  std::cout << (se::StorageEngine::getInstance().select("test")).dump();
+  std::cout << (se::StorageEngine::getInstance().select("test_problem")).dump();
   return 0;
 }
