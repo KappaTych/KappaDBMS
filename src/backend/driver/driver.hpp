31fa81402b5d56f5bd00c66712f1b02420445113
Anton Podchuvalov <podchuvalovvl@gmail.com>
2018-11-10 01:38:25 +1000

    Init Driver Class

---
+++ src/backend/driver/driver.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class Driver {
+public:
+    static Driver& Instance() {
+        static Driver instance;
+        return instance;
+    }
+
+    std::string handle(std::string) {};
+private:
+    Driver() = default;
+};
