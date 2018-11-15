2aa7f244055d8d3c1bf6affbc697bb2e21825c9c
Anastasia <pototckaia@gmail.com>
2018-11-15 21:31:36 +1000

    Add table definition to drop table

--- src/backend/parser/sql-stmt/DropTable.hpp
+++ src/backend/parser/sql-stmt/DropTable.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
 #include "Instruction.hpp"
+#include "TableDefinition.hpp"
 
 
 namespace cmd {
 
-class DropTable : public Instruction
-{
+class DropTable : public Instruction {
 public:
-  DropTable() : Instruction(DROP_TABLE) {}
-  ~DropTable() = default;
+  DropTable() = default;
+  explicit DropTable(cmd::TableDefinition table) : Instruction(DROP_TABLE), table_(std::move(table)) {}
+  ~DropTable() override = default;
 
-  const Instruction& Dispatch() const override { return *this; }
+  const DropTable& Dispatch() const override { return *this; }
+
+public:
+  cmd::TableDefinition table_;
 };
 
 } // namespace cmd
