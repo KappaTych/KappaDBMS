a2d21a14b1f13487b8dd9fda360e7cc376594428
Anastasia <pototckaia@gmail.com>
2018-11-15 21:45:15 +1000

    Fix return type ColumnDefinition

--- src/backend/parser/sql-stmt/ColumnDefinition.hpp
+++ src/backend/parser/sql-stmt/ColumnDefinition.hpp
@@ -1,19 +1,19 @@
-#pragma once 
+#pragma once
 
 #include "Instruction.hpp"
+#include "Literal.hpp"
 
 
 namespace cmd {
 
 class ColumnDefinition : public Instruction
-{
-public:
-  ColumnDefinition(const std::string s, cmd::LiteralType t)
-      : Instruction(COLUMN_DEF),
-        name_(s),
-        type_(t) { }
+ {
+ public:
+  ColumnDefinition(const std::string& s, cmd::LiteralType t)
+    : Instruction(COLUMN_DEF), name_(s), type_(t) {}
 
-  const Instruction& Dispatch() const override { return *this; }
+
+  const ColumnDefinition& Dispatch() const override { return *this; }
 
 public: // It's not an error
   std::string name_;
