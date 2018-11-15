b739e6d161f022cc65145ed91e2656e46d070b51
Anastasia <pototckaia@gmail.com>
2018-11-15 21:41:48 +1000

    Remove database from TableDefinition

--- src/backend/parser/sql-stmt/TableDefinition.hpp
+++ src/backend/parser/sql-stmt/TableDefinition.hpp
@@ -8,16 +8,14 @@ namespace cmd {
 class TableDefinition : public Instruction
 {
 public:
-  TableDefinition(const std::string& n, const std::string& d = "default", const std::string& s = "public")
-      : Instruction(TABLE_DEF),
-        database_(d),
-        schema_(s),
-        name_(n) { }
+  explicit TableDefinition(std::string n, std::string s = "public")
+      : Instruction(TABLE_DEF), schema_(std::move(s)), name_(std::move(n)) {}
 
-  const Instruction& Dispatch() const override { return *this; }
+  TableDefinition() = default;
+
+  const TableDefinition& Dispatch() const override { return *this; }
 
 public: // It's not an error
-  std::string database_;
   std::string schema_;
   std::string name_;
 };
