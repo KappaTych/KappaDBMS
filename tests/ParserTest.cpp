0a7b3e4426820c7061d65ffb4447948b917ab4d4
Anastasia <pototckaia@gmail.com>
2018-11-16 12:20:35 +1000

    Add new valid statement

--- tests/ParserTest.cpp
+++ tests/ParserTest.cpp
@@ -8,23 +8,47 @@ int main(int argc, char *argv[])
   std::string s;
   auto& parser = sql::Parser::Instance();
 
-  s = "Drop table t";
-  parser.Process(s);
+  s = "Drop table t;";
+  auto tree = parser.Process(s);
 
   s = "show table stud";
-  parser.Process(s);
+  tree = parser.Process(s);
 
   s = "CREATE TABLE students (name TEXT) ";
-  parser.Process(s);
+  tree = parser.Process(s);
 
   s = "CREATE TABLE students (id Text, id1 TEXT, idddd TEXT) ; ";
-  parser.Process(s);
+  tree = parser.Process(s);
 
   s = "CREATE TABLE students (id DOUBLE) ; ";
-  parser.Process(s);
+  tree = parser.Process(s);
 
   s = "CREATE TABLE students (id INTEGER)";
-  parser.Process(s);
+  tree = parser.Process(s);
+
+  s = "SElECT * from t";
+  tree = parser.Process(s);
+
+  s = "select a, b, c from t";
+  tree = parser.Process(s);
+
+  s = "select * from t where t == ee;";
+  tree = parser.Process(s);
+
+  s = "select * from t where t == 'dd'";
+  tree = parser.Process(s);
+
+  s = "select * from t where t > 900.02";
+  tree = parser.Process(s);
+
+  s = "select * from t where t > 900";
+  tree = parser.Process(s);
+
+  s = "insert into table_name values (123, 'test', 12.1); ";
+  tree = parser.Process(s);
+
+  s = "insert into table_name field1, field2 values ('1', '2');";
+  tree = parser.Process(s);
 
   return 0;
 }
