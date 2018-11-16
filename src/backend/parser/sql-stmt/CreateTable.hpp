2c5d818e79c3284c8542de9da4d61c03ed114662
Anastasia <pototckaia@gmail.com>
2018-11-16 10:15:18 +1000

    Add file to CreateTable

--- src/backend/parser/sql-stmt/CreateTable.hpp
+++ src/backend/parser/sql-stmt/CreateTable.hpp
@@ -2,23 +2,21 @@
 
 #include "Instruction.hpp"
 #include "TableDefinition.hpp"
-
+#include "ColumnDefinition.hpp"
 
 namespace cmd {
 
 class CreateTable : public Instruction
 {
 public:
-  CreateTable(TableDefinition& t)
-      : Instruction(CREATE_TABLE),
-        table_(t) {}
+  CreateTable(TableDefinition t, std::list<cmd::ColumnDefinition> columns)
+    : Instruction(CREATE_TABLE), table_(std::move(t)), columns_(std::move(columns)) {}
 
-
-  const Instruction& Dispatch() const override { return *this; }
+  const CreateTable& Dispatch() const override { return *this; }
 
 public: // It's not an error
-  TableDefinition& table_;
-  std::list<Instruction*> columns_;
+  TableDefinition table_;
+  std::list<cmd::ColumnDefinition> columns_;
 };
 
 } // namespace cmd
