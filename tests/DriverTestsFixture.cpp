71fc909c99cd3aee0580417b8e40461c9a3a5670
GlushkovVK <glushkov.vk@students.dvfu.ru>
2019-01-10 23:50:45 +1000

    Add some tests

--- tests/DriverTestsFixture.cpp
+++ tests/DriverTestsFixture.cpp
@@ -4,7 +4,6 @@
 
 #include <gtest/gtest.h>
 #include <driver/driver.hpp>
-#include <gmock/gmock.h>
 
 namespace {
     class DriverTestsFixture : public ::testing::Test {
--- tests/ParserTestsFixture.cpp
+++ tests/ParserTestsFixture.cpp
@@ -3,11 +3,16 @@
 //
 
 #include <gtest/gtest.h>
+#include <parser/parser.hpp>
+#include <memory.h>
+#include <string>
 
 namespace {
     class ParserTestsFixture : public ::testing::Test {
     public:
         ParserTestsFixture() {}
+
+        sql::Parser &parser = sql::Parser::Instance();
 
         virtual void SetUp() {}
 
@@ -19,6 +24,34 @@ namespace {
     };
 }
 
-TEST_F(ParserTestsFixture, TEST1) {
-    ASSERT_EQ(1, 1);
+TEST_F(ParserTestsFixture, CREATE_TABLE_TEST) {
+    // init test input and expected results
+    std::vector<std::shared_ptr<cmd::Instruction>> instructions;
+    std::vector<std::string> create_table_instructions{
+            "CREATE TABLE students(id INTEGER);",
+    };
+
+    std::vector<cmd::InstructionType> expected_instruction_types{
+            cmd::InstructionType::CREATE_TABLE,
+    };
+
+    std::vector<std::vector<std::string>> expected_definitions{
+            {"default", "public", "students"},
+    };
+
+    //parse queues and get instructions
+    for (const auto &create_table_instruction : create_table_instructions) {
+        instructions.push_back(parser.Process(create_table_instruction)[0]);
+    }
+
+
+    //check got instructions data and expected data
+    for (int i = 0; i < instructions.size(); ++i) {
+        ASSERT_EQ(expected_instruction_types[i], instructions[i].get()->type());
+
+        ASSERT_EQ(expected_definitions[i][0], dynamic_cast<cmd::CreateTable*>(instructions[0].get())->table_.database_);
+        ASSERT_EQ(expected_definitions[i][1], dynamic_cast<cmd::CreateTable*>(instructions[0].get())->table_.schema_);
+        ASSERT_EQ(expected_definitions[i][2], dynamic_cast<cmd::CreateTable*>(instructions[0].get())->table_.name_);
+
+    }
 }
