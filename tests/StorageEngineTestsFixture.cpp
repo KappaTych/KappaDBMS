c52e91fd247802944b2ba5c2a81f2f96a358d29c
nixtaxe <fenix_dz@mail.ru>
2019-01-21 01:47:34 +1000

    Add Update, Delete test

--- tests/StorageEngineTestsFixture.cpp
+++ tests/StorageEngineTestsFixture.cpp
@@ -11,9 +11,11 @@ namespace
 class StorageEngineTestsFixture : public ::testing::Test
 {
 public:
+
   ClassToTestCodeCoverage obj;
   se::StorageEngine& storage = se::StorageEngine::Instance();
   std::string tableName = "some_table";
+  int size;
 
   StorageEngineTestsFixture()
   {}
@@ -23,7 +25,6 @@ public:
     // Supported types info
     std::unordered_map<std::string, se::size_t> mapping = {{"INTEGER", sizeof(int32_t)},
                                                            {"TEXT",    se::STRING_LEN},};
-
     if (!storage.HasMetaData(tableName)) {
       // CREATE TABLE
       auto& meta = storage.CreateData(tableName);
@@ -55,13 +56,9 @@ public:
 };
 }
 
-TEST_F(StorageEngineTestsFixture, TEST_CREATE_METADATA)
+TEST_F(StorageEngineTestsFixture, TEST1)
 {
   ASSERT_TRUE(storage.HasMetaData(tableName));
-}
-
-TEST_F(StorageEngineTestsFixture, TEST_INSERT_AND_SELECT)
-{
   // INSERT INTO
   auto& meta = storage.GetMetaData(tableName);
   se::size_t size = meta.data()["private"]["size"];
@@ -80,10 +77,8 @@ TEST_F(StorageEngineTestsFixture, TEST_INSERT_AND_SELECT)
   raw.FullReset().Fill<int32_t>(first_expected[2]).Fill<std::string>(second_expected[2]).Fill<int32_t>(third_expected[2]);
   storage.Write(meta, raw.data(), raw.capacity());
 
-  // SELECT *
-  std::cout << "SELECT ALL" << std::endl;
+  // SELECT ALL
   auto dataAll1 = storage.Read(meta, size);
-
   int i = 0;
   for (auto& x : dataAll1) {
     ASSERT_EQ(x.Get<int32_t>(), first_expected[i]);
@@ -91,4 +86,54 @@ TEST_F(StorageEngineTestsFixture, TEST_INSERT_AND_SELECT)
     ASSERT_EQ(x.Get<int32_t>(), third_expected[i++]);
     x.Reset();
   }
+
+  meta = storage.GetMetaData(tableName);
+  // SELECT * WHERE id = 2
+  int expected_id = 2;
+  auto dataWhere = storage.Read(meta, size, [](const se::RawData& raw) {
+    return (raw.Get<int>() == 2);
+  });
+
+  ASSERT_EQ(dataWhere.size(), 1);
+
+  auto& result = *dataWhere.begin();
+  ASSERT_EQ(result.Get<int32_t>(), expected_id);
+  result.Reset();
+
+  // UPDATE WHERE id = 1
+  expected_id = 1;
+  const std::string expected_line = "Updated first line!";
+  storage.Update(meta, size, [](se::RawData&& raw) {
+    if (raw.Get<int>() == 1) {
+      raw.Fill<std::string>("Updated first line!", true);
+      return true;
+    }
+    return false;
+  });
+
+  dataWhere = storage.Read(meta, size, [](const se::RawData& raw) {
+    return (raw.Get<int>() == 1);
+  });
+
+  ASSERT_EQ(dataWhere.size(), 1);
+
+  result = *dataWhere.begin();
+  ASSERT_EQ(result.Get<int32_t>(), expected_id);
+  ASSERT_EQ(result.Get<std::string>(), expected_line);
+  result.Reset();
+
+  // DELETE WHERE count < 64
+  int border = 64;
+  storage.Delete(meta, size, [](const se::RawData& raw) {
+    return (raw.Skip<int>().Skip<std::string>(se::STRING_LEN).Get<int>() < 64);
+  });
+
+  dataAll1 = storage.Read(meta, size);
+  for (auto& x : dataAll1) {
+    x.Get<int32_t>();
+    x.Get<std::string>();
+    ASSERT_GE(x.Get<int32_t>(), border);
+    x.Reset();
+  }
+
 }
